176e059cd6abaea291963199842582f707795b48
Maxime Duhamel <max.duh22@gmail.com>
2025-10-04 13:57:33 +0200

    feat: change close button to match Mac's buttons

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -69,9 +69,9 @@ function DialogContent({
         {showCloseButton && (
           <DialogPrimitive.Close
             data-slot="dialog-close"
-            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 cursor-pointer"
+            className="ring-offset-background focus:ring-ring bg-red-500 data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-full p-[1px] opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 cursor-pointer group/close"
           >
-            <XIcon />
+            <XIcon className="duration-500 opacity-0 group-hover/close:opacity-100 size-3" />
             <span className="sr-only">Close</span>
           </DialogPrimitive.Close>
         )}
