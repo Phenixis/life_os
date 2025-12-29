29fb4629b37a57b5ab32af6957bed76b30f6b911
Maxime Duhamel <max.duh22@gmail.com>
2025-12-29 11:11:49 +0100

    fix: correct import path for DailyMoodModal in ModalManager

--- components/big/modal-manager.tsx
+++ components/big/modal-manager.tsx
@@ -2,7 +2,7 @@
 
 import TaskModal from "@/components/big/tasks/task-modal"
 import NoteModal from "@/components/big/notes/note-modal"
-import DailyMoodModal from "@/components/big/dailyMood/dailyMood-modal"
+import DailyMoodModal from "@/components/big/daily-mood/daily-mood-modal"
 
 export default function ModalManager() {
     return (
