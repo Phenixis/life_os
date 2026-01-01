bd8d62ab91a347636a483faa6e2884a6a9ab4a6e
Maxime Duhamel <max.duh22@gmail.com>
2026-01-01 18:14:17 +0100

    feat: implement PasswordInput component

--- components/big/calendar/calendar.tsx
+++ components/big/calendar/calendar.tsx
@@ -187,7 +187,7 @@ export default function Calendar({
   return (
     <div
       className={cn(
-        'sticky top-0 flex flex-col justify-start items-start border-l border-gray-100 dark:border-gray-800 p-2 pb-4 h-fit max-h-[80dvh] overflow-y-auto md:pb-0 md:items-center md:w-full md:h-screen md:max-h-screen md:max-w-75 transition-opacity duration-300',
+        'sticky top-0 flex flex-col justify-start items-start border-l border-gray-100 dark:border-gray-800 p-2 pb-4 h-fit max-h-[80dvh] overflow-y-auto md:pb-2 md:items-center md:w-full md:h-screen md:max-h-screen md:max-w-75 transition-opacity duration-300',
         className
       )}
       style={{
